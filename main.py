2601754ec442d868b66611d8dd4f61e93460e038
Francisco Prestamo <prestamo2003@gmail.com>
2023-12-26 07:41:29 -0500

    agregando proyecto 1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 
 def home():
     st.title("Home Page")
@@ -108,12 +109,55 @@ def function5():
         st.write("Parameter 2:", param2)
         # Add your function 5 logic here
 
+def logistic_map(r, x0, n):
+    """
+    Calculates the first n terms of the logistic map.
+
+    Args:
+        r: The bifurcation parameter.
+        x0: The initial value.
+        n: The number of terms to calculate.
+
+    Returns:
+        A NumPy array containing the first n terms of the logistic map.
+    """
+    x = np.zeros(n)
+    x[0] = x0
+    for i in range(1, n):
+        x[i] = r * x[i-1] * (1 - x[i-1])
+        print(f"x{i} = {x[i]}")
+    return x
+
+def proyect_1():
+    min_r = 0.0
+    max_r = 4.0
+
+    r_value = st.slider("R value", value=min_r, max_value=max_r, step=0.000001)
+    st.write("value of r:", r_value)
+    print(f"{r_value}")
+
+    min_x = 0.0
+    max_x = 1.0
+
+    x_value = st.slider("X0 value", value=min_x, max_value=max_x, step=0.000001)
+    st.write("value of x0:", x_value)
+    print(f"{x_value}")
+
+    n_terms = 100
+
+    def update():
+        x = logistic_map(r_value, x_value, n_terms)
+        for i in range(100):
+            x[i] = x[i]
+        line = st.line_chart(x)
+    update()
 
 # Create a dictionary of page names and corresponding functions
 pages = {
     "Home": home,
     "About Us": about,
-    "Function Selection": function_selection
+    "Function Selection": function_selection,
+    "Proyecto 1": proyect_1
 }
 
 # Create a sidebar to navigate between pages
